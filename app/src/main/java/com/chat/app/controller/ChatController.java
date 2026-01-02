5f271de4461f6b0b05a8ba489d4a033943643dec
Manish Raje <manishbraje@gmail.com>
2026-01-02 16:51:16 +0530

    chat messages displaying

--- app/src/main/java/com/chat/app/controller/ChatController.java
+++ app/src/main/java/com/chat/app/controller/ChatController.java
@@ -1,13 +1,26 @@
 package com.chat.app.controller;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 import com.chat.app.model.ChatMessage;
+import org.springframework.web.bind.annotation.GetMapping; 
+
 
 @Controller
-
 public class ChatController {
-    public ChatMessage sendMessage(ChatMessage messgage) {
-        return messgage;
+
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/messages")
+    public ChatMessage sendMessage(@Payload ChatMessage message) {
+        return message;
     }
+
+    @GetMapping("chat")
+    public String chat() {
+        return "chat";
+    }
+    
 }
